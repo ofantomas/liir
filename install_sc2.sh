06db6ceb55b1fb136c5676ca2e6037d77d888f30
ofantomas <tsypinartem9@gmail.com>
2022-02-22 01:13:06 +0300

    Another requirements fix

--- install_sc2.sh
+++ install_sc2.sh
@@ -17,9 +17,9 @@ echo 'SC2PATH is set to '$SC2PATH
 
 if [ ! -d $SC2PATH ]; then
         echo 'StarCraftII is not installed. Installing now ...';
-        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.2.69232.zip
-        unzip -P iagreetotheeula SC2.4.6.2.69232.zip
-        rm -rf SC2.4.6.2.69232.zip
+        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
+        unzip -P iagreetotheeula SC2.4.10.zip
+        rm -rf SC2.4.10.zip
 else
         echo 'StarCraftII is already installed.'
 fi
@@ -33,10 +33,9 @@ if [ ! -d $MAP_DIR ]; then
 fi
 
 cd ..
-wget https://github.com/oxwhirl/smac/files/2760783/SMAC_Maps.zip
+wget https://github.com/oxwhirl/smac/releases/download/v0.1-beta1/SMAC_Maps.zip
 unzip SMAC_Maps.zip
 mv SMAC_Maps $MAP_DIR
 rm -rf SMAC_Maps.zip
 
 echo 'StarCraft II and SMAC are installed.'
-
